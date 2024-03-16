f60242cf7fab69f3a009da12da2998d9d4148a6b
Jasjot Singh <53977657+JasjotSingh001@users.noreply.github.com>
2024-03-16 12:52:56 +0000

    Live server works

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'src/'),
+      directory: path.join(__dirname, 'public/'),
     },
     compress: true,
     port: 9000,
