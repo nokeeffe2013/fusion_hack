f3b6f1ec3d2eb5752d54691255e9f3e4973c6eff
Jasjot Singh <53977657+JasjotSingh001@users.noreply.github.com>
2024-03-16 18:44:48 +0000

    Basic game logic

---
+++ public/js/card.js
@@ -0,0 +1,12 @@
+class Card {
+    constructor(id, age, socialMedia, areaPop, areaWealth, pos, totalPower) {
+        this.age = age;
+        this.socialMedia = socialMedia;
+        this.areaPop = areaPop;
+        this.areaWealth = areaWealth;
+        this.pos = pos;
+        this.totalPower;
+    }
+}
+
+export default Card;
