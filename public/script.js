a467a239dfe3e0ee9337f40afdc3bf467788de6e
Niamh <okeeffe57@gmail.com>
2024-03-16 18:52:45 +0000

    bug fixer

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 var total_points = 1000;
 
+let draggedItems = [];
+
 function updatePointsDisplay() {
     document.getElementById("pointsValue").textContent = total_points ;
   }
@@ -12,7 +14,11 @@ function allowDrop(event) {
   }
   
   function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
+    if (!draggedItems.includes(event.target.id)) {
+        event.dataTransfer.setData("text", event.target.id);
+      } else {
+        event.preventDefault();
+      }
   }
   
   function drop(event) {
@@ -22,10 +28,19 @@ function allowDrop(event) {
     var clone = draggedElement.cloneNode(true) 
     var playerPoints = parseInt(draggedElement.getAttribute('points') || 0);
 
+    if(event.target != event.currentTarget) {
+        var location = draggedItems.indexOf(event.currentTarget);
+        var playerPoints = parseInt(event.currentTarget.getElementById(data).getAttribute('points') || 0);
+        updatePoints(-playerPoints);
+        draggedItems.splice(location, 1)
+    }
+
+    
     if(total_points - playerPoints >= 0) {
         updatePoints(playerPoints);
         event.target.appendChild(clone)
     }
+    draggedItems.push(data)
   }
 
   function updatePoints(amount) {
